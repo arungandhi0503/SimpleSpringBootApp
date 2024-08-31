a736898d7150a1f5015900e7f3f4f07ed57a58c7
arungandhi0503 <61384255+arungandhi0503@users.noreply.github.com>
2024-08-31 23:36:41 +0200

    First Commit

---
+++ src/main/java/com/agcode/agcart/AgcartApplication.java
@@ -0,0 +1,13 @@
+package com.agcode.agcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agcode/agcart/AgcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agcode.agcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
